66694626914544cabf51d35157bc16cb5859b2bc
Ana Clara Mansano <anacmans@outlook.com>
2022-01-12 22:55:15 -0300

    Moviecard

--- frontend/src/components/pages/Listing/index.tsx
+++ frontend/src/components/pages/Listing/index.tsx
@@ -1,10 +1,32 @@
+import MovieCard from "../../MovieCard";
 import Pagination from "../../Pagination";
 
-function Listing () {
+function Listing() {
+  return (
+    <>
+      <Pagination />
 
-    return (
-        <Pagination />
-    );
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Listing;
