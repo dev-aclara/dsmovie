fba2606e0376e290d60fbd159e5b1642da0d8ccc
Ana Clara Mansano <anacmans@outlook.com>
2022-01-15 00:53:01 -0300

    useState e useEffect

--- frontend/src/components/pages/Listing/index.tsx
+++ frontend/src/components/pages/Listing/index.tsx
@@ -1,19 +1,30 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
+import { BASE_URL } from "utils/requests";
 import MovieCard from "../../MovieCard";
 import Pagination from "../../Pagination";
 
 function Listing() {
 
+  const [pageNumber, setPageNumber] = useState(0);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&pages=1`)
+    .then(response => {
+      const data = response.data as MoviePage;
+      setPageNumber(data.number);
+    });
+
+  }, []);
 
   // FORMA ERRADA
-  axios.get('https://dev-aclara-dsmovie.herokuapp.com/movies/2')
-    .then(response => {
-      console.log(response.data);
-    });
+  
 
 
   return (
     <>
+    <p>{pageNumber}</p>
       <Pagination />
 
       <div className="container">
