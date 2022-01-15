3a79d820e40951b693cd6bffa480d1b9faac9a90
Ana Clara Mansano <anacmans@outlook.com>
2022-01-15 11:16:43 -0300

    Pagination

--- frontend/src/components/pages/Listing/index.tsx
+++ frontend/src/components/pages/Listing/index.tsx
@@ -6,44 +6,56 @@ import MovieCard from "../../MovieCard";
 import Pagination from "../../Pagination";
 
 function Listing() {
+
   const [pageNumber, setPageNumber] = useState(0);
 
   const [page, setPage] = useState<MoviePage>({
-    content: [],
-    last: true,
-    totalPages: 0,
-    totalElements: 0,
-    size: 12,
-    number: 0,
-    first: true,
-    numberOfElements: 0,
-    empty: true,
+      content: [],
+      last: true,
+      totalPages: 0,
+      totalElements: 0,
+      size: 12,
+      number: 0,
+      first: true,
+      numberOfElements: 0,
+      empty: true
   });
 
   useEffect(() => {
-    axios
-      .get(`${BASE_URL}/movies?size=12&pages=${pageNumber}`)
-      .then((response) => {
-        const data = response.data as MoviePage;
-        setPage(data);
-      });
+      axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+          .then(response => {
+              const data = response.data as MoviePage;
+              setPage(data);
+          });
   }, [pageNumber]);
 
-  return (
-    <>
-      <Pagination />
+  const handlePageChange = (newPageNumber : number) => {
+      setPageNumber(newPageNumber);
+  }
 
-      <div className="container">
-        <div className="row">
-          {page.content.map((movie) => (
-            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-              <MovieCard movie={movie} />
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
+  return (
+
+      <>
+
+          <Pagination page={page} onChange={handlePageChange} />
+
+          <div className="container">
+              <div className="row">
+                  {page.content.map(movie => (
+                      <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                          <MovieCard movie={movie} />
+                      </div> 
+                  )
+                  )}
+
+              </div>
+
+          </div>
+
+
+      </>
   );
+
 }
 
 export default Listing;
