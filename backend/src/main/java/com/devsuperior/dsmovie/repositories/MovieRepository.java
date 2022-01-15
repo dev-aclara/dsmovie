0ddc4717c34bb640dc722ecf3effd05049e5e4d3
Ana Clara Mansano <anacmans@outlook.com>
2022-01-14 21:54:57 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/repositories/MovieRepository.java
+++ backend/src/main/java/com/devsuperior/dsmovie/repositories/MovieRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmovie.entities.Movie;
 
-public interface MovieRepository extends JpaRepository <Movie, Long> {
+public interface MovieRepository  extends JpaRepository<Movie, Long> {
 
 }
